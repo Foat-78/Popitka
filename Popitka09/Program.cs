4747e5f19fe0ad17c395a52c7030c1c74ae966f9
Foat Khisamov <foat.com>
2023-06-29 22:58:39 +1000

    Работа с текстом.

---
+++ Popitka09/Program.cs
@@ -0,0 +1,37 @@
+﻿// Работа с текстом.
+// Дан текст. В тексте нужно все пробелы заменить чёрточками, 
+// маленькие буквы "к" заменить на большие  буквы "К", а большие 
+// "С" заменить маленькими "с".
+ 
+ string text = "Приветствую вас на лекции по введению в программирование. Мы продолжаем разбираться " 
+            + "с методами. На предыдущем занятии мы узнали, для чего они нужны. Узнали, что они, как " 
+            + "правило, решают подзадачи. Сегодня мы углубимся в тему и выделим 4 основных группы " 
+            + "методов, а также будем использовать их на практике. И познакомимся с новыми языковыми " 
+            + "конструкциями, в частности, циклом. Приступим.";
+
+// Пояснение:
+// string s = "qwerty"
+//             012
+// s[3] = r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replase(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(text, 'к', 'К');
+Console.WriteLine(newText);
+newText = Replase(text, 'С', 'с');
+Console.WriteLine(newText);
+
