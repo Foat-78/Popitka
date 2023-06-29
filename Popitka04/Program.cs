a80e94bf530ab303bb64ca556fc149278f616df6
Foat Khisamov <foat.com>
2023-06-29 22:44:14 +1000

    Метод 2. чего-то принимает и ничего не возвращает

--- Popitka04/Program.cs
+++ Popitka04/Program.cs
@@ -4,3 +4,14 @@ void Method2(string msg)
     Console.Write(msg);
 }
 Method2("Текст для Фоата .....");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        ++i;
+    }
+}
+Method21("Фоат !!!!!!", 4);
