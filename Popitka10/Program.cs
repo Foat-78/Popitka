639104c9ab81b83a7f095081dc2f57a54d94fa4a
Foat Khisamov <foat.com>
2023-07-09 14:50:10 +1000

    Работа с массивами

--- Popitka10/Program.cs
+++ Popitka10/Program.cs
@@ -1,39 +1,39 @@
 ﻿// Работа с массивами
 
 
-int [] arr = {1, 2, 5, 6, 3, 9, 2, 1};
+// int [] arr = {1, 2, 5, 6, 3, 9, 2, 1};
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
 
-void SelectionSort(int [] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if(array[j] < array[minPosition]) minPosition =j;
-        }
+// void SelectionSort(int [] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition]) minPosition =j;
+//         }
 
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-}
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
 
 
-PrintArray(arr);
-SelectionSort(arr);
+// PrintArray(arr);
+// SelectionSort(arr);
 
-PrintArray(arr);
+// PrintArray(arr);
 
 int [] arr = {1, 2, 5, 6, 3, 9, 2, 1};
 
